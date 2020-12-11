d458014761dab7cef686f3ba4a6ac6ebf9d7d830
felix-oss <uv.mhjl@gmail.com>
2020-12-11 14:28:34 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Clock } from "./clock.js";
 
 //create your first component
 export function Home() {
-	return <div className="text-center mt-5" />;
+	return (
+		<span className="text-center mt-5 d-flex d-inline justify-content-center">
+			<Clock />
+			<Clock />
+			<Clock />
+			<Clock />
+			<Clock />
+		</span>
+	);
 }
